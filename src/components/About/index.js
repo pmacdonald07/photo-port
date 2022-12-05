10a8277a54d83585877c412075f89d476a0cf778
Patrick M <patrickmacdonald07@gmail.com>
2022-12-05 17:57:51 -0500

    added about and nav components

---
+++ src/components/About/index.js
@@ -0,0 +1,17 @@
+import coverImage from "../../assets/cover/cover-image.jpg";
+
+const About = () => {
+  return (
+    <section className="my-5">
+      <h1>Who am I?</h1>
+      <img
+        src={coverImage}
+        className="my-2"
+        style={{ width: "100%" }}
+        alt="cover"
+      />
+    </section>
+  );
+};
+
+export default About;
---
+++ src/components/Nav/index.js
@@ -0,0 +1,52 @@
+const categories = [
+  {
+    name: "commercial",
+    description:
+      "Photos of grocery stores, food trucks, and other commercial projects",
+  },
+  { name: "portraits", description: "Portraits of people in my life" },
+  { name: "food", description: "Delicious delicacies" },
+  {
+    name: "landscape",
+    description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+  },
+];
+// "Note also the use of parentheses in the map callback to return JSX.
+// When you map over an array in a JSX expression, you should return only a single JSX element—
+// like how you can only return a single element from a React component."
+const Nav = () => {
+  const categorySelected = (name) => {
+    console.log(`${name} clicked`);
+  };
+  return (
+    <header className="flex-row">
+      <h2>
+        <a href="/">
+          <span role="img" aria-label="camera">
+            📸
+          </span>{" "}
+          Oh Snap!
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          <li className="mx-2">
+            <a href="#about">About me</a>
+          </li>
+          <li>
+            <span>Contact</span>
+          </li>
+          {categories.map((category) => (
+            <li className="mx-1" key={category.name}>
+              <span onClick={() => categorySelected(category.name)}>
+                {category.name}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
