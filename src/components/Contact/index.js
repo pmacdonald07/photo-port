45f09dd643d18bbf4e584ab820452207ddf493b9
Patrick M <patrickmacdonald07@gmail.com>
2022-12-07 15:53:08 -0500

    added form handling for contact form

---
+++ src/components/Contact/index.js
@@ -0,0 +1,59 @@
+import { useState } from "react";
+
+const ContactForm = () => {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const { name, email, message } = formState;
+
+  // We use the spread operator, ...formState, so we can retain the other key-value pairs in this object.
+  // Without the spread operator, the formState object would be overwritten to only contain the name: value key pair.
+  const handleChange = (e) => {
+    setFormState({ ...formState, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(formState);
+  };
+
+  return (
+    <section>
+      <h1>Contact Me</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            name="name"
+            defaultValue={name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input
+            type="email"
+            name="email"
+            defaultValue={email}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea
+            name="message"
+            rows="5"
+            defaultValue={message}
+            onChange={handleChange}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+};
+
+export default ContactForm;
